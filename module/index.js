bbb0f2afc88dae2a78dec566f0540cdc95db09bd
abhinav3254 <abhinav3254@gmail.com>
2022-04-02 10:52:38 +0530

    module

---
+++ module/index.js
@@ -0,0 +1,14 @@
+// Those module which we have to import is called non-global module
+
+// import filesystem
+
+const FileSystem = require('fs');
+
+console.log("hi Abhinav");
+
+//  and console is global module which we don't have to import
+
+FileSystem.writeFileSync("hello.txt","Hii this is the tutorial of node js which i am learning from youtube and youtube channel name is code step by step");
+
+
+FileSystem.writeFileSync("hello2.txt","node js is an backend tool it is not an programming language");
