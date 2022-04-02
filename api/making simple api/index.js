efb052b81b0cb4f389712434e0ffb0590153caf9
abhinav3254 <abhinav3254@gmail.com>
2022-04-02 13:56:19 +0530

    API

--- api/making simple api/index.js
+++ api/making simple api/index.js
@@ -6,6 +6,7 @@ http.createServer((request,response) => {
     // since we are sending data here so we will write responce
     // if we take the data then we will write request
     response.writeHead(200,{'Content-Type':'application\json'});
+    // 200 is status and there are various status
     // writing the body
     response.write(JSON.stringify(dataArray));
     // ending the responce that we have finished our work
