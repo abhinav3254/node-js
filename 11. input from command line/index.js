32923c0e8dbfba9ca23236a067c080aa34f9ef58
abhinav3254 <abhinav3254@gmail.com>
2022-04-07 14:20:35 +0530

    Advacing The Module

--- 11. input from command line/index.js
+++ 11. input from command line/index.js
@@ -4,4 +4,14 @@ const fs = require('fs');
 
 const value = process.argv;
 
-fs.writeFileSync(value[2],value[3]);
+// fs.writeFileSync(value[2],value[3]);
+
+// Making it little bit Advance
+
+if(value[2] == 'add') {
+    fs.writeFileSync(value[3],value[4]);
+} else if(value[2] == 'remove') {
+    fs.unlinkSync(value[3]);
+} else {
+    console.log('Invalid Ibnput');
+}
