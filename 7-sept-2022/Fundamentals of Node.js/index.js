6fa96b9cc9134fc321f110d0b0dec52878ebc9cb
abhinav3254 <abhinav3254@gmail.com>
2022-09-07 15:02:45 +0530

    new commit

---
+++ 7-sept-2022/Fundamentals of Node.js/index.js
@@ -0,0 +1,14 @@
+var x = 10;
+let y = 50;
+const z = 99;
+
+x = 2929;
+y = 1212;
+// z = 8989;
+
+// Here we can't reassign the value in the const type of variables.
+
+
+console.log(x+y+z);
+
+console.log(x);
