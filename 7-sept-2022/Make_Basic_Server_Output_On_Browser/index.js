eac21826ec7586a562b21cbb8eaa7544df265f41
abhinav3254 <abhinav3254@gmail.com>
2022-09-07 19:42:03 +0530

    arrow function

--- 7-sept-2022/Make_Basic_Server_Output_On_Browser/index.js
+++ 7-sept-2022/Make_Basic_Server_Output_On_Browser/index.js
@@ -3,19 +3,34 @@
 
 const http = require('http');
 
-http.createServer((request,response) => {
-	response.write("<h1>Hello World</h1>");
-	response.end();
-}).listen(9595);
+// 1. One way of creating a server using arrow function
 
+// http.createServer((request,response) => {
+// 	response.write("<h1>Hello World</h1>");
+// 	response.end();
+// }).listen(9595);
+
+
+
+// 2. Second way using another function like a noob programmer basically for me :)
+// Describing the arrow function
+
+function dataControlOne(request,response) {
+	response.write("<h1>Hello This is Abhinav Jha</h1>");
+	response.end();
+}
+
+http.createServer(dataControlOne).listen(4500);
 
 
 
 // request -> Takes something from the client using url
 // response -> sends something to the client
 
-// test(int a , int b) {
+// ************ Assume this is function and we have to change it in arrow function so what we have to do is *************//
 
+// function test(int a ) {
+	// return a*10;
 // }
 
-// test(10,20);
+// const test = (a) => a*10;
