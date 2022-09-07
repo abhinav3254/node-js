a166accb2c2bb91d77368b1486fbf582457afb20
abhinav3254 <abhinav3254@gmail.com>
2022-09-07 18:42:28 +0530

    core modules and global modules

---
+++ 7-sept-2022/core_modules_in_node.js/index.js
@@ -0,0 +1,13 @@
+// Core modules
+
+
+// console.log("Abhinav Jha");
+
+// Here console is already made in the pacakge and we don't have to make any of them so that's the reason we call it as core modules.
+
+// FileSystem , Buffer , HTTP these all are our core modules.
+
+//2. What are golbal modules.
+
+//  Global modules are those kind of modules whom we don't have to import
+// exmaple console.log() --> we don't have to import these modules so these are called as global modules.
