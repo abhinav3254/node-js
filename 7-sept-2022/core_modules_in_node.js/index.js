250475186a5b50ad1894267e5853e3b6efdd4c54
abhinav3254 <abhinav3254@gmail.com>
2022-09-07 18:49:07 +0530

    global and non-global modules

--- 7-sept-2022/core_modules_in_node.js/index.js
+++ 7-sept-2022/core_modules_in_node.js/index.js
@@ -11,3 +11,13 @@
 
 //  Global modules are those kind of modules whom we don't have to import
 // exmaple console.log() --> we don't have to import these modules so these are called as global modules.
+
+const fs = require('fs');
+
+fs.writeFileSync("Hello.txt","This file is generated using fs.writeFileSync");
+
+// Here fs is the non-global module and we have to import fs.
+// Those modules whom we have to import is called as non-global.
+
+console.log("Directory name is ------>  "+__dirname);
+console.log("File name is ------> ",__filename);
