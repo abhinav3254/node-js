f1f09613b718f858a231248cad1b16424d3ae8e1
abhinav3254 <abhinav3254@gmail.com>
2022-09-08 07:15:01 +0530

    practices

---
+++ 7-sept-2022/practices/Hello.js
@@ -0,0 +1,8 @@
+// Hello this is Abhinav Jha learning nodes.js
+
+module.exports = {
+	name : 'Abhinav',
+	age : '22',
+	dob : '0x-0x-xxxx',
+	contact : 'xxxxxxxx@xmail.com'
+}
---
+++ 7-sept-2022/practices/index.js
@@ -0,0 +1,18 @@
+// const http = require('http');
+
+// http.createServer((request,response) => {
+// 	response.write("<h1>Hello this is abhinav jha</h1>");
+// 	response.end();
+// }).listen(4500);
+
+
+
+// Making a file or writing a file
+
+// const fs = require('fs');
+
+// fs.writeFileSync("Hello.js","Hello this is Abhinav Jha learning nodes.js");
+
+const hello = require('./Hello');
+
+console.log(hello);
