f2208d4e8de0e487f3ca209401a6c540ad59ca5e
abhinav3254 <abhinav3254@gmail.com>
2022-09-12 20:08:36 +0530

    use of rpomise to overcome async issue

--- 7-sept-2022/async-basics-in-node.js/index.js
+++ 7-sept-2022/async-basics-in-node.js/index.js
@@ -24,12 +24,25 @@
 let x = 20;
 let y = 0;
 
-setTimeout(() => {
-    y = 100;
-}, 2000);
+// setTimeout(() => {
+//     y = 100;
+// }, 2000);
 
-console.log(x+y);
+// console.log(x+y);
 
 // Output of this code is 20
 
 // This is the drwaback of the node.js that it will print 20 instead of 120 because it was not able to update th value.
+
+
+let waitingData = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve(100);
+    }, 2000);
+});
+
+waitingData.then((data)=>{
+    x = data;
+    console.log(x+y);
+})
+// console.log(x+y);
--- 7-sept-2022/async-basics-in-node.js/readme.md
+++ 7-sept-2022/async-basics-in-node.js/readme.md
@@ -4,3 +4,6 @@ Node is async
 js is async
 
 php is sync langauge
+
+Time stamp : - 2:31:00
+date and time of creation :- 12 sept 2022 and time is 20:00 pm.
