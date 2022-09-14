a426133a461102614db62b40bcda4c80bf273fee
abhinav3254 <abhinav3254@gmail.com>
2022-09-14 08:36:39 +0530

    how node works

---
+++ 7-sept-2022/how-nodejs-works/index.js
@@ -0,0 +1,13 @@
+console.log('Startin');
+
+setTimeout(()=>{
+    console.log('2 sec log');
+},2000);
+
+setTimeout(()=>{
+    console.log('0 sec log');
+},0);
+
+console.log('Finishing Up');
+
+// firstly line 1 execute then line 11 and then 7 then 3
