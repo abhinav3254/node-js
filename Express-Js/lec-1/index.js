3a5d0b4977bfb2d30b2fa58a2c4318d66f9c184c
abhinav3254 <abhinav3254@gmail.com>
2022-09-14 09:11:18 +0530

    data sent from the user or request management

--- Express-Js/lec-1/index.js
+++ Express-Js/lec-1/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('',(request,response)=>{
+    console.log("data sent from the user is :- ",request.query);
     response.send('Hello this is Home Page');
 });
 
