e3a75e6375c463c79dacbc97cf97a7d144191b4b
abhinav3254 <abhinav3254@gmail.com>
2022-09-15 11:49:32 +0530

    age>18 logic in middleware

--- Express-Js/Middleware/index.js
+++ Express-Js/Middleware/index.js
@@ -8,6 +8,18 @@ const app = express();
 // }
 
 
+// Making the use of the middleware in real life
+
+const reqFilter = (req,res,next) => {
+    if (!req.query.age) {
+        res.send('<h1>Enter your age in the query box</h1>');
+    } else if (req.query.age<18){
+        res.send('<h1>sorry your age is below 18</h1>')
+    }else {
+        next();
+    }
+}
+
 app.use(reqFilter);
 
 app.get('/',(req,res)=>{
