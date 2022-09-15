22490e71d1ae5a216c6488a073fa0fd7a2a09dec
abhinav3254 <abhinav3254@gmail.com>
2022-09-15 20:57:25 +0530

    moving to another file

--- mongoDB/read-data-from-mongodb-using-node/index.js
+++ mongoDB/read-data-from-mongodb-using-node/index.js
@@ -1,37 +1,6 @@
-const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost:27017'
-const database = 'e-comm';
 
-const client = new MongoClient(url);
-
-// async function getData() {
-//     let result = await client.connect();
-//     let db = result.db(database);
-//     let collection = db.collection('products');
-//     let response = await collection.find({}).toArray();
-//     console.log(response);
-// }
-
-
-// To find a specific result from the database
-async function dbConnect() {
-    let result = await client.connect();
-    let db = result.db(database);
-    // let collection = db.collection('products');
-    return db.collection('products');
-    // let response = await collection.find({model:'k2'}).toArray();
-    // console.log(response);
-}
-
-
-// One way of handling promise in node-js
-
-// dbConnect().then((res)=>{
-//     // console.warn(res.find().toArray())
-//     res.find({}).toArray().then((data)=>{
-//         console.warn(data);
-//     })
-// })
+const db = require('./mongodb');    
+// const db = require('../mongodb'); //on the global level   
 
 
 // Easy and 2nd way to handle async
