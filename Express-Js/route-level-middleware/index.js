fd1b10fcc1a37c6d53292b110910a87f2b11bba9
abhinav3254 <abhinav3254@gmail.com>
2022-09-15 12:28:34 +0530

    shifiting middleware to another file

--- Express-Js/route-level-middleware/index.js
+++ Express-Js/route-level-middleware/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const app = express();
 
-const reqFilter = (req,res,next) => {
-    if (!req.query.age) {
-        res.send('<h1>Enter your age in the query box</h1>');
-    } else if (req.query.age<18){
-        res.send('<h1>sorry your age is below 18</h1>')
-    }else {
-        next();
-    }
-}
+const reqFilter = require('./middleware');
 
 // app.use(reqFilter);
 
