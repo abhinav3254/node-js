70f254da70dc19222458fbe5bcfe8e960327b28a
abhinav3254 <abhinav3254@gmail.com>
2022-09-15 11:42:30 +0530

    making middleware

--- Express-Js/Middleware/index.js
+++ Express-Js/Middleware/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Making a middleware
+const reqFilter = (req,res,next)=>{
+    console.log('reqFilter');
+    next();
+}
+
+app.use(reqFilter);
+
 app.get('/',(req,res)=>{
 res.send('welcome to home page');
 });
