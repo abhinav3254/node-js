6f6768536b276667ed53061c28f83ccff00daa54
abhinav3254 <abhinav3254@gmail.com>
2022-09-15 20:51:38 +0530

    Easy and 2nd way to handle async

--- mongoDB/read-data-from-mongodb-using-node/index.js
+++ mongoDB/read-data-from-mongodb-using-node/index.js
@@ -26,10 +26,20 @@ async function dbConnect() {
 
 // One way of handling promise in node-js
 
-dbConnect().then((res)=>{
-    // console.warn(res.find().toArray())
-    res.find({}).toArray().then((data)=>{
-        console.warn(data);
-    })
-})
-console.log(dbConnect());
+// dbConnect().then((res)=>{
+//     // console.warn(res.find().toArray())
+//     res.find({}).toArray().then((data)=>{
+//         console.warn(data);
+//     })
+// })
+
+
+// Easy and 2nd way to handle async
+const main = async () => {
+    // console.log("main function called");
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.warn(data);
+}
+// console.log(dbConnect());
+main();
