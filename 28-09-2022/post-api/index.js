71c7a06c8b3aa3f0eb5f3ed4f8d0fc2b08ecb408
abhinav3254 <abhinav3254@gmail.com>
2022-09-28 21:31:37 +0530

    get-request

--- 28-09-2022/post-api/index.js
+++ 28-09-2022/post-api/index.js
@@ -7,6 +7,14 @@ const app = express();
 // middle ware used here
 app.use(express.json());
 
+
+app.get('/', async (req, res) => {
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data);
+    res.send(data);
+})
+
 app.post('/', async (req, res) => {
     console.log(req.body)
 
