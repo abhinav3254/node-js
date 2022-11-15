9c7f212ddf8c6d48962be1d0020f15f0b2d7e67d
abhinav3254 <abhinav3254@gmail.com>
2022-11-15 14:03:35 +0530

    use of modules

--- Nov2022/test-module/names.js
+++ Nov2022/test-module/names.js
@@ -1,5 +1,7 @@
 const privateData = 'Private Data';
 
+// using modules we can't access this private data and if we don't use modules then this data will be accessible for all the 
+
 const abhinav = 'abhinav';
 const aman = 'aman';
 
